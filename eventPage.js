f13babe3769352ad270dd019a2c9378866aeeb77
guragainroshan0 <guragainroshan0@gmail.com>
2019-01-27 20:32:56 +0545

    documented content.js

--- eventPage.js
+++ eventPage.js
@@ -25,28 +25,28 @@ chrome.storage.sync.get(['title'],function(object){
     
 });
 
-var menuItem = {
-    "id":"report_link",
-    "title":"Report Link",  
-};
-chrome.contextMenus.create(menuItem);
+// var menuItem = {
+//     "id":"report_links",
+//     "title":"Report Link",  
+// };
+// chrome.contextMenus.create(menuItem);
 
-chrome.contextMenus.onClicked.addListener(function(clickData){
-    if(clickData.menuItemId=="report_link")
-    {
+// chrome.contextMenus.onClicked.addListener(function(clickData){
+//     if(clickData.menuItemId=="report_link")
+//     {
         
-            var url_d = clickData.pageUrl;
-            var xhr  =new XMLHttpRequest();
-            xhr.open("GET","http://localhost:8888/report?url="+url_d);
-            xhr.onload = function(){
-                console.log(xhr.response);
-            }
-            xhr.send();
+//             var url_d = clickData.pageUrl;
+//             var xhr  =new XMLHttpRequest();
+//             xhr.open("GET","http://localhost:8888/report?url="+url_d);
+//             xhr.onload = function(){
+//                 console.log(xhr.response);
+//             }
+//             xhr.send();
         
      
         
-    }
-});
+//     }
+// });
 
 
 chrome.tabs.query({active:true,currentWindow:true},function(tabs){
